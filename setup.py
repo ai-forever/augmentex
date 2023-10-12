e12dae41fad506ff6926e290bd7887a211cf2c58
Баушенко Марк Андреевич <e0xextazy@gmail.com>
2023-10-12 22:20:48 +0300

    update setup.py for release 1.1.0

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from glob import glob
 
 setup(
     name="augmentex",
-    version="1.0.0",
+    version="1.1.0",
     author="Mark Baushenko and Alexandr Abramov",
     author_email="m.baushenko@gmail.com",
     description="Augmentex — a library for augmenting texts with errors",
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/ai-forever/augmentex",
     packages=find_packages(),
-    data_files=[('static_data', glob('augmentex/static_data/*.json'))],
+    data_files=[("static_data", glob("augmentex/static_data/**/*.json", recursive=True))],
     include_package_data=True,
     classifiers=[
         "Natural Language :: English",
@@ -26,7 +26,7 @@ setup(
         "Topic :: Text Editors :: Text Processing",
     ],
     python_requires=">=3.7.0",
-    install_requires=['numpy>=1.21'],
+    install_requires=["numpy>=1.21", "python-Levenshtein>=0.22.0"],
     keywords="augmentex errors typos nlp augmentation",
     zip_safe=False,
 )
